a177bfbee770cdc7bdeab6f265f33e39b9813832
Adam Kaufman <adamkaufman@Adams-MacBook-Pro-3.local>
2022-11-04 16:40:07 -0400

    tashon's data

--- routes/artists.js
+++ routes/artists.js
@@ -1,25 +1,24 @@
 import { Router } from "express";
-import * as controllers from "../controllers/artists.js"
+import * as controllers from "../controllers/artists.js";
 
+const router = Router();
 
-const router = Router()
-
-router.get("/", controllers.getArtists)
-router.get("/:id", controllers.getArtist)
-router.get("/artist/:name", controllers.getArtistName)
-router.get("/artist/genre/:genre", controllers.getArtistGenre)
-router.post("/", controllers.createArtist)
-router.put("/:id", controllers.updateArtist)
-router.put("/artist/:name", controllers.updateArtistName)
-router.delete("/:id", controllers.deleteArtist)
-router.delete("/artist/:name", controllers.deleteArtistName)
+router.get("/artists", controllers.getArtists);
+router.get("/:id", controllers.getArtist);
+router.get("/artist/:name", controllers.getArtistName);
+router.get("/artist/genre/:genre", controllers.getArtistGenre);
+router.post("/artists", controllers.createArtist);
+router.put("/:id", controllers.updateArtist);
+router.put("/artist/:name", controllers.updateArtistName);
+router.delete("/:id", controllers.deleteArtist);
+router.delete("/artist/:name", controllers.deleteArtistName);
 
 // Albums
 
-router.get("/albums/:title", controllers.getAlbums)
-router.get("/album/:title", controllers.getAlbumTitle)
-router.post("/album/:title", controllers.createAlbum)
-router.put("/album/:title", controllers.updateAlbumTitle)
-router.delete("/album/:title", controllers.deleteAlbumName)
+router.get("/artists/albums/all", controllers.getAlbums);
+router.get("/album/:title", controllers.getAlbumTitle);
+router.post("/album/:title", controllers.createAlbum);
+router.put("/album/:title", controllers.updateAlbumTitle);
+router.delete("/album/:title", controllers.deleteAlbumTitle);
 
-export default router
+export default router;
