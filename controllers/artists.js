cb320149b7a178b12da5b9c292b3e6a03bc9b3dc
Adam Kaufman <adamkaufman@Adams-MacBook-Pro-3.local>
2022-11-09 16:12:12 -0500

    fixed add update and delete

--- controllers/artists.js
+++ controllers/artists.js
@@ -125,93 +125,93 @@ export const deleteArtistName = async (req, res) => {
 
 // Albums
 
-export const getAlbums = async (req, res) => {
-  try {
-    const albums = await Album.find();
-    res.json(albums);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error.message });
-  }
-};
+// export const getAlbums = async (req, res) => {
+//   try {
+//     const albums = await Album.find();
+//     res.json(albums);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: error.message });
+//   }
+// };
 
-export const getAlbumTitle = async (req, res) => {
-  try {
-    const { title } = req.params;
-    const album = await Album.find({ title: title });
+// export const getAlbumTitle = async (req, res) => {
+//   try {
+//     const { title } = req.params;
+//     const album = await Album.find({ title: title });
 
-    if (album) {
-      return res.json(album);
-    }
+//     if (album) {
+//       return res.json(album);
+//     }
 
-    res.status(404).json({ message: "Album not found!" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error.message });
-  }
-};
+//     res.status(404).json({ message: "Album not found!" });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: error.message });
+//   }
+// };
 
-export const createAlbum = async (req, res) => {
-  try {
-    const album = new Album(req.body);
-    await album.save();
-    res.status(201).json(album);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error.message });
-  }
-};
+// export const createAlbum = async (req, res) => {
+//   try {
+//     const album = new Album(req.body);
+//     await album.save();
+//     res.status(201).json(album);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: error.message });
+//   }
+// };
 
-export const updateAlbum = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const album = await Album.findByIdAndUpdate(id, req.body);
-    res.status(201).json(album);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error.message });
-  }
-};
+// export const updateAlbum = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const album = await Album.findByIdAndUpdate(id, req.body);
+//     res.status(201).json(album);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: error.message });
+//   }
+// };
 
-export const updateAlbumTitle = async (req, res) => {
-  try {
-    const { title } = req.params;
-    const album = await Album.findOneAndUpdate({ title: title }, req.body);
-    res.status(201).json(album);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error.message });
-  }
-};
+// export const updateAlbumTitle = async (req, res) => {
+//   try {
+//     const { title } = req.params;
+//     const album = await Album.findOneAndUpdate({ title: title }, req.body);
+//     res.status(201).json(album);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: error.message });
+//   }
+// };
 
-export const deleteAlbum = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deleted = await Album.findByIdAndDelete(id);
+// export const deleteAlbum = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const deleted = await Album.findByIdAndDelete(id);
 
-    if (deleted) {
-      return res.status(200).send("Album deleted!");
-    }
+//     if (deleted) {
+//       return res.status(200).send("Album deleted!");
+//     }
 
-    throw new Error("Album not found");
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error.message });
-  }
-};
+//     throw new Error("Album not found");
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: error.message });
+//   }
+// };
 
-export const deleteAlbumTitle = async (req, res) => {
-  try {
-    const { title } = req.params;
-    const deleted = await Album.findOneAndDelete({ title: title });
+// export const deleteAlbumTitle = async (req, res) => {
+//   try {
+//     const { title } = req.params;
+//     const deleted = await Album.findOneAndDelete({ title: title });
 
-    if (deleted) {
-      return res.status(200).send("Album deleted!");
-    }
+//     if (deleted) {
+//       return res.status(200).send("Album deleted!");
+//     }
 
-    throw new Error("Album not found");
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error.message });
-  }
-};
+//     throw new Error("Album not found");
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: error.message });
+//   }
+// };
