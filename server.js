cf4c6813f3b1132c931f3fd81f600202ec24a099
Lauren Powers <laurenpowers@Laurens-Air.lan>
2022-12-15 17:05:58 -0500

    change port

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-import db from "./db/connection.js"
-import routes from "./routes/index.js"
-import express from "express"
-import cors from "cors"
-import logger from "morgan"
-import chalk from "chalk"
+import db from "./db/connection.js";
+import routes from "./routes/index.js";
+import express from "express";
+import cors from "cors";
+import logger from "morgan";
+import chalk from "chalk";
 
+const app = express();
+const port = process.env.PORT || `0.0.0.0:$PORT`;
 
-const app = express()
-const port = process.env.PORT || 3000
+app.use(express.json());
+app.use(cors());
+app.use(logger("dev"));
 
-app.use(express.json())
-app.use(cors())
-app.use(logger("dev"))
-
-app.use("/", routes)
+app.use("/", routes);
 
 db.on("connected", () => {
-  console.clear()
+  console.clear();
   console.log(chalk.blue("Connected to MongoDB!"));
   app.listen(port, () => {
     console.log(`Express server running in development on port ${port}`);
-  })
-})
+  });
+});
